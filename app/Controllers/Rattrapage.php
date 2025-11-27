8d8f4c44aaa3bbb0847f47924d47eb8df1d229ba
Rémi <remiviez@gmail.com>
2025-11-27 17:33:53 +0100

    save rattrapage contoller

--- app/Controllers/Rattrapage.php
+++ app/Controllers/Rattrapage.php
@@ -101,6 +101,7 @@ class Rattrapage extends BaseController
     /**
      * Sauvegarde d'un nouveau rattrapage
      */
+    /*
     public function save($idDs)
     {
         $session = session();
@@ -170,7 +171,87 @@ class Rattrapage extends BaseController
 
         return redirect()->to('Rattrapage')->with('success', 'Rattrapage ajouté avec succès');
     }
+    */
 
+    public function save($idDs)
+    {
+        $request = \Config\Services::request();
+        $post = $request->getPost();
+
+        $studentModel = new StudentsModel();
+        $perPage = max((int) ($this->request->getGet('perPage') ?? 10), 1);
+        $keyword = $this->request->getGet('keyword') ?? '';
+        $students = $studentModel->getPaginatedStudentsByDSiD($idDs, $perPage, $keyword);
+
+        $justifies = $post['justify'] ?? [];
+        foreach ($students as $s) {
+            $sid = $s['id'];
+            if (!isset($justifies[$sid])) {
+                $justifies[$sid] = 'off';
+            }
+        }
+        $post['justify'] = $justifies;
+
+        $rules = [
+            'semester' => 'required|in_list[S1]',
+            'resource' => 'required|in_list[R1.05 blabla,R1.02 blibli]',
+            'teacher' => 'required|in_list[Legrix,Thorel]',
+            'date' => 'required|regex_match[/^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[012])\/\d{2}$/]',
+            'hour' => 'required|regex_match[/^(?:[01]\d|2[0-3]):[0-5]\d$/]',
+            'type' => 'required|in_list[MACHINE,PAPIER,ORAL]',
+            'duration' => 'required|regex_match[/^(?:[01]\d|2[0-3]):[0-5]\d$/]',
+            'room' => 'required|alpha_numeric_space|max_length[3]|min_length[3]'
+        ];
+
+    $validation = \Config\Services::validation();
+    $validation->setRules($rules);
+    $isValid = $validation->run($post);
+
+        foreach ($post['justify'] as $val) {
+            if (!in_array($val, ['on', 'off'], true)) {
+                $validation->setError('justify', 'Valeur invalide pour justify');
+                $isValid = false;
+                break;
+            }
+        }
+
+        if (!$isValid) {
+            $data = [
+                'validation' => $validation,
+                'DSInformation' => array_merge($post, ['idDs' => $idDs]),
+                'types' => ['MACHINE' => 'MACHINE', 'ORAL' => 'ORAL', 'PAPIER' => 'PAPIER'],
+                'students' => $students,
+                'keyword' => $keyword,
+                'old_justify' => $post['justify'] ?? []
+            ];
+
+            return view('rattrapage/ajout', $data);
+        }
+
+        $informations = $post;
+
+        $duration = substr($informations['duration'], 0, 2) * 60 + substr($informations['duration'], 3, 2);
+
+        $data = [
+            'id_ds' => (int) $idDs,
+            'code' => $informations['codeEnseignant'],
+            'date_rattrapage' => $informations['date'],
+            'duree_minutes' => $duration,
+            'heure_debut' => $informations['hour'],
+            'etat' => 'EN ATTENTE',
+            'mail_envoye' => 0,
+            'type_exam' => $informations['type'],
+            'salle' => $informations['room']
+        ];
+
+        $rattrapageModel = new RattrapageModel();
+        $rattrapageModel->insertRattrapage($data);
+
+        $dsModel = new DsModel();
+        $dsModel->setEtat($idDs, 'PREVU');
+    
+        return view('rattrapage');
+    }
     /**
      * Détail d'un rattrapage
      */
